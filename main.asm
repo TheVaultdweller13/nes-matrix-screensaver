985842b7e8477e29a313a67a4ff2a27df42d1190
Héctor Fernández Hompanera <geralt741@gmail.com>
2024-05-24 19:17:29 +0200

    add simple sprite movement

--- main.asm
+++ main.asm
@@ -4,6 +4,9 @@
   .inesmap 0 ; mapper 0 = NROM, no bank swapping
   .inesmir 1 ; background mirroring
 ; -----------------------------------------------------------
+  .rsset $0000
+  
+player_x .rs 1
 
   .bank 0
   .org  $8000
@@ -11,7 +14,7 @@ RESET:
   SEI        ; ignore IRQs
   CLD        ; disable decimal mode
   LDX #$40
-  STX $4017  ; disable APU frame IRQ
+  STX $4017 ; disable APU frame IRQ
   LDX #$ff
   TXS        ; Set up stack
   INX        ; now X = 0
@@ -54,7 +57,7 @@ LoadBackgroundPaletteLoop:
   LDA   background_palette, x     ; Load palette byte
   STA   $2007                     ; Write to PPU
   INX                             ; Set index to next byte
-  CPX   #$20                      ; Check against 32 (hex 30)
+  CPX   #$20
   BNE   LoadBackgroundPaletteLoop ;	If x = $20, 32 bytes copied, all done
   LDX #$00
 
@@ -65,54 +68,72 @@ LoadSpritePaletteLoop:
   CPX #$10            
   BNE LoadSpritePaletteLoop ; if x = $10, all done
 
-LoadSpritesTitle:
+LoadSpriteTitle:
   LDX #$00                ; start at 0
-LoadSpritesTitleLoop:
-  LDA sprites_title, x          ; Load data from address (sprites + x)
+LoadSpriteTitleLoop:
+  LDA sprite_title, x          ; Load data from address (sprites + x)
   STA $0200, x            ; Store into RAM address ($0200 + x)
   INX                     ; Increment X
   CPX #$30                ; Check against 48 (hex 30)
-  BNE LoadSpritesTitleLoop     ; If x = $30, 48 bytes copied, all done
+  BNE LoadSpriteTitleLoop     ; If x = $30, 48 bytes copied, all done
+
+  LDA #$10          ; enable sprites
+  STA $2001
+
+LoadSpriteCharacter:
+  LDX #$00
+LoadSpriteCharacterLoop:
+  LDA sprite_character, x
+  STA $0230, x
+  INX
+  CPX #$10
+  BNE LoadSpriteCharacterLoop
+
+  LDA $0201
+  STA player_x
+
   
+;------
   LDA #$88          ; enable NMI, sprites from Pattern Table 2 ($80 from pattern table 1)
   STA $2000
 
   LDA #$10          ; enable sprites
   STA $2001
+
 ; -------------------------------------------
 ; ----------------- AUDIO -------------------
-GenerateSound:
-; Notes reference:
-; NTSC: https://nerdy-nights.nes.science/downloads/missing/NotesTableNTSC.txt
-; PAL:  https://nerdy-nights.nes.science/downloads/missing/NotesTablePAL.txt
-
-  LDA #%10111111 ;Duty 10, Length Counter Disabled, Saw Envelopes disabled, Volume F
-  STA $4000
-
-LoadNotes:
-  ; CHANNEL SQUARE 1
-  LDA #$1
-  STA $4015
-  LDX #$00
-
-  LDA #$00
-  STA $4003
-
-LoadNotesLoop:
-  LDA notes, x
-  STA $4002
-    
-  INX
-
-  JSR delay
-  JSR delay
-
-  CPX #$7
-  BNE LoadNotesLoop
-
-; END CHANNEL
-  LDA #%0000000
-  STA $4015
+;;;; GenerateSound:
+;;;; ; Notes reference:
+;;;; ; NTSC: https://nerdy-nights.nes.science/downloads/missing/NotesTableNTSC.txt
+;;;; ; PAL:  https://nerdy-nights.nes.science/downloads/missing/NotesTablePAL.txt
+;;;; 
+;;;;   LDA #%10111111 ;Duty 10, Length Counter Disabled, Saw Envelopes disabled, Volume F
+;;;;   STA $4000
+;;;; 
+;;;; LoadNotes:
+;;;;   ; CHANNEL SQUARE 1
+;;;;   LDA #$1
+;;;;   STA $4015
+;;;;   LDX #$00
+;;;; 
+;;;;   LDA #$00
+;;;;   STA $4003
+;;;; 
+;;;; LoadNotesLoop:
+;;;;   LDA notes, x
+;;;;   STA $4002
+;;;;     
+;;;;   INX
+;;;; 
+;;;;   JSR delay
+;;;;   JSR delay
+;;;; 
+;;;;   CPX #$7
+;;;;   BNE LoadNotesLoop
+;;;; 
+;;;; ; END CHANNEL
+;;;;   LDA #%0000000
+;;;;   STA $4015
 
 ; -------------------------------------------
 
@@ -146,24 +167,35 @@ NMI:
   LDA   #$02
   STA   $4014 ; set the high byte (02) of the RAM address, start the transfer
 
+  LDA player_x
+  STA $0233
+  STA $023B
+  TAX
+  CLC
+  ADC #$08
+  STA $0237
+  STA $023F
+  INX
+  STX player_x
+
   RTI         ; return from interrupt
 ; -------------------------------------------
   .bank 1
   .org  $E000
 ; ---------- PALETTES AND SPRITES ----------
 background_palette:
-  .db $22 ,$29 ,$1A ,$0F	;background palette 1
-  .db $22 ,$36 ,$17 ,$0F	;background palette 2
-  .db $22 ,$30 ,$21 ,$0F	;background palette 3
-  .db $22 ,$27 ,$17 ,$0F	;background palette 4
+  .db $22 ,$29 ,$1A ,$0F       ;background palette 1
+  .db $22 ,$36 ,$17 ,$0F       ;background palette 2
+  .db $22 ,$30 ,$21 ,$0F       ;background palette 3
+  .db $22 ,$27 ,$17 ,$0F       ;background palette 4
   
 sprite_palette:
-  .db $22 ,$16 ,$27 ,$18	;sprite palette 1
-  .db $22 ,$1A ,$30 ,$27	;sprite palette 2
-  .db $22 ,$16 ,$30 ,$27	;sprite palette 3
-  .db $22 ,$0F ,$36 ,$17	;sprite palette 4
-  
-sprites_title:
+  .db $22 ,$16 ,$27 ,$18       ;sprite palette 1
+  .db $22 ,$1A ,$30 ,$27       ;sprite palette 2
+  .db $22 ,$16 ,$30 ,$27       ;sprite palette 3
+  .db $22 ,$0F ,$36 ,$17       ;sprite palette 4
+   
+sprite_title:
   ; Y Position, Tile number, Attributes, X Position
   .db $60,  $1E,  $00,  $42   ; U
   .db $60,  $17,  $00,  $51   ; N
@@ -179,6 +211,13 @@ sprites_title:
   .db $80,  $18,  $00,  $7E   ; O
   .db $80,  $0B,  $00,  $8A   ; B
   .db $80,  $18,  $00,  $96   ; O
+
+sprite_character:
+  ; Y Position, Tile number, Attributes, X Position
+  .db $10, $B0, $00, $10   ;sprite 0
+  .db $10, $B2, $00, $18   ;sprite 1
+  .db $18, $B1, $00, $10   ;sprite 0
+  .db $18, $B3, $00, $18   ;sprite 1
 
 ; C, D, E, F, G, A, B.
 notes:
