d4f332643b17c8631061023893fbbb9accf9102a
Héctor Fernández Hompanera <geralt741@gmail.com>
2024-05-26 14:18:02 +0200

    chore: Update readme and improvement compile file for windows

--- compile.bat
+++ compile.bat
@@ -1,2 +1,24 @@
-NESASM3 main.asm
+@echo off
+echo.
+echo Compiling...
+ca65 registers.inc
+ca65 main.asm
+if not %errorlevel% == 0 (
+    echo Error: Failed to compile
+    pause
+    exit /b %errorlevel%
+)
+echo - Compiled successfully.
+
+echo.
+
+echo Linking...
+ld65 registers.o main.o -C nes.cfg -o main.nes
+if not %errorlevel% == 0 (
+    echo Error: Failed to link
+    pause
+    exit /b %errorlevel%
+)
+echo - Linked successfully.
+echo.
 pause
